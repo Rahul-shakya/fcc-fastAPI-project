3d6ffda3390bd35387fb35f532c0d8dcb125ffb5
Rahul Shakya <rahul.shakya3@gmail.com>
2024-09-12 13:53:38 -0500

    Added POST request

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ app = FastAPI()
 async def root():
     return {"message" : "This is a drill."}
 
+@app.get("/test_get")
+async def root():
+    return {"message" : "This is a test."}
+
+@app.post("/create_post")
+def create_post():
+    return {"message" : "This is a post"}
