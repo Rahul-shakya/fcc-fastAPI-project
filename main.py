22e249c48834886ec8846c065c1cb71f69d9081e
Rahul Shakya <rahul.shakya3@gmail.com>
2024-09-12 16:28:32 -0500

    Schema validation through pydantic

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+
+class Post(BaseModel):
+    title: str
+    content: str
+
 app = FastAPI()
 
 @app.get("/")
@@ -11,7 +17,12 @@ async def root():
     return {"message" : "This is a test."}
 
 @app.post("/create_post")
-def create_post(payLoad: dict = Body(...)):
+def create_post(payLoad: Post):
 
     # this is how we extract the data from body of the payload 
-    return {"title" : f"title is {payLoad['title']}", "content": f"Content is {payLoad['content']}"}
+    # return {"title" : f"title is {payLoad['title']}", "content": f"Content is {payLoad['content']}"}
+
+    # using pydantic, simple printing does the job, we dont have to do like before |
+    print(payLoad)
+    print(f"{payLoad.title}  {payLoad.content}")
+    return True
