193b80d7b9f3500c199fb6940e6082748ecf2da3
Rahul Shakya <rahul.shakya3@gmail.com>
2024-09-15 17:12:52 -0500

    Changes server to default_server.

--- app/models.py
+++ app/models.py
@@ -7,6 +7,6 @@ class Post(Base):
     id = Column(Integer, primary_key = True, nullable = False)
     title = Column(String, nullable = False)
     content = Column(String, nullable = False)
-    published = Column(Boolean, default = False)
+    published = Column(Boolean, server_default = 'False', nullable = False)
     rating = Column(Integer, nullable = True)
     
