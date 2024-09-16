a6408773e93da1cad92141dd0eb671ed24a36e16
Rahul Shakya <rahul.shakya3@gmail.com>
2024-09-16 16:23:55 -0500

    Added pydantic model for user

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Optional
 from datetime import datetime
 
@@ -27,4 +27,27 @@ class Post(PostBase):
         # orm_mode = True
 
         # this is new change
+        from_attributes = True
+
+
+class UserCreate(BaseModel):
+
+    user_name: str
+    email: EmailStr
+    password: str
+
+    # we dont include id and created_at because those
+    # values are provided by the system and not the user.
+    # Here we define only the values which the user will
+    # send in the body of the request.
+
+
+# we dont want to send password back to the user hence including 
+# only id and email
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+    
+    class Config:
         from_attributes = True
