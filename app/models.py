7ed10f4e514b1f904ab1ce21856e113ec9ed2901
Rahul Shakya <rahul.shakya3@gmail.com>
2024-09-16 15:07:07 -0500

    Created user SQL Alchemy model

--- app/models.py
+++ app/models.py
@@ -3,7 +3,8 @@ from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 
-# this is the SQL Alchemy model
+# this is the SQL Alchemy model. Creating these models and running the main file creates
+# tables in the db based on the attributes defined in the class
 
 class Post(Base):
     __tablename__ = 'tb_posts'
@@ -13,4 +14,14 @@ class Post(Base):
     content = Column(String, nullable = False)
     is_published = Column(Boolean, server_default = 'False', nullable = False)
     rating = Column(Integer, nullable = True)
+    created_at = Column(TIMESTAMP(timezone = True), nullable = False, server_default = text('now()'))
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key = True, nullable = False)
+    user_name = Column(String, nullable = False)
+    email = Column(String, nullable = False, unique = True)
+    password = Column(String, nullable = False)
     created_at = Column(TIMESTAMP(timezone = True), nullable = False, server_default = text('now()'))
