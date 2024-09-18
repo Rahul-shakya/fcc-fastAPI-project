bb4fcf606ebcc76909cc55f893d1ca5ab8b52035
Rahul Shakya <rahul.shakya3@gmail.com>
2024-09-18 16:22:35 -0500

    Implemented Foriegn Key constraint for tb_posts

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 
@@ -15,6 +15,8 @@ class Post(Base):
     is_published = Column(Boolean, server_default = 'False', nullable = False)
     rating = Column(Integer, nullable = True)
     created_at = Column(TIMESTAMP(timezone = True), nullable = False, server_default = text('now()'))
+
+    user_id = Column(Integer, ForeignKey('users.id', ondelete = 'CASCADE'), nullable = False)
 
 
 class User(Base):
