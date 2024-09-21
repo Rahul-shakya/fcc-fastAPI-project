0a114b38ece5171bc9d258868324367d18c70f97
Rahul Shakya <rahul.shakya3@gmail.com>
2024-09-21 17:32:23 -0500

    Table creation done through Alembic only, not through SQL Alchemy

--- app/mainDBStoreORM.py
+++ app/mainDBStoreORM.py
@@ -4,7 +4,12 @@ from .database import engine
 from .routers import like, post, user, auth
 
 # this line actually creates the tables through SQLAlchemy
-models.Base.metadata.create_all(bind = engine)
+# models.Base.metadata.create_all(bind = engine)
+# Since table creation has been implemented to Alembic,using create_all in the code becomes redundant. 
+# Alembic will handle the generation and application of SQL schema changes when migrations are executed, 
+# based on versioned migration scripts. If create_all is kept in the code, it might create the tables 
+# before Alembic runs, rendering the first migration unnecessary, 
+# But anyways commenting it out!
 
 
 app = FastAPI()
